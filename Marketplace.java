270440dac1295650fbfa03eb145ab265aa9c98e5
russherm <russherm@iupui.edu>
2017-02-02 21:43:04 -0500

    Adding Marketplace

---
+++ Marketplace.java
@@ -0,0 +1,9 @@
+/**
+ * RMI Interface for Online Marketplace
+ * @author Russell Sherman
+ *
+ */
+public interface Marketplace extends java.rmi.Remote {
+	int get_id() throws java.rmi.RemoteException;
+
+}
