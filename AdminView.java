125eaaac41deed261f94b5d667571cfe604f48ca
russherm <russherm@iupui.edu>
2017-02-02 21:41:07 -0500

    Adding AdminView

---
+++ AdminView.java
@@ -0,0 +1,15 @@
+import java.rmi.Naming;
+
+/**
+ * Marketplace Admin View 
+ * @author Russell Sherman
+ *
+ */
+public class AdminView {
+
+	public void registerListener(AdminController acontroller) {
+		// Do something
+		
+	}
+	
+}
