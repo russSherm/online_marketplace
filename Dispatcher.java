6cc0ed5f74f997b966a86f12c8e3209d8c747655
russherm <russherm@iupui.edu>
2017-02-16 21:31:35 -0500

    Adding Dispatcher

---
+++ Dispatcher.java
@@ -0,0 +1,30 @@
+/**
+ * Dispatcher Class for Front Controller
+ * @author Russell Sherman
+ */
+public class Dispatcher {
+	//Identify Concrete Views
+	private CustomerView customerView;
+	private AdminView adminView;
+	
+	/**
+	 * Construct Dispatcher
+	 */
+	public Dispatcher() {
+		customerView = new CustomerView();
+		adminView = new AdminView();
+	}
+	
+	/**
+	 * Dispatch correct view based on login credentials
+	 */
+	public void dispatch(String request) {
+		if(request.equalsIgnoreCase("Ryan")) {
+			adminView.showView();
+		}
+		else {
+			customerView.showView();
+		}
+	}
+
+}
