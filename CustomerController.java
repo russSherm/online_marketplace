747ffbec2e1d24b230c227001147e2080ed4ca0e
russherm <russherm@iupui.edu>
2017-02-16 21:30:34 -0500

    Adding revised Customer Controller

--- CustomerController.java
+++ CustomerController.java
@@ -8,8 +8,6 @@ import java.rmi.Naming;
 public class CustomerController {
 	private MarketplaceModel model;
 	private CustomerView view;
-	//Customer ID Number
-	static int id;
 	
 	//Bring together model and view
 	public CustomerController (MarketplaceModel model, CustomerView view){
@@ -17,32 +15,4 @@ public class CustomerController {
 		this.view = view;
 	}
 	
-	public static void main(String args[]){
-		//Java RMI Security Manager
-		System.setSecurityManager(new SecurityManager());
-		
-		//Use Try-Catch for remote exceptions
-		try {
-			/*
-			 * Show hostname where RMI Server is running.
-			 * Hard code the port to avoid using default 1099
-			 */
-			String name = "//tesla.cs.iupui.edu:1720/MarketplaceServer";
-			
-			//Locate Marketplace Server
-			Marketplace myMarketplace = (Marketplace) Naming.lookup(name);
-			
-			//Call remote method on Marketplace Server
-			id = myMarketplace.get_id();
-			
-			System.out.println("Welcome to the Marketplace! Have a look around!");
-			System.out.println("Your Customer Id is: " + id);
-		} catch(Exception e){
-			System.out.println("CustomerView Exception: " + e.getMessage());
-			e.printStackTrace();
-		}
-		
-	}
-
-
 }
