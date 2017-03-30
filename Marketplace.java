639b0a6b0ab1b2fa9c1c34dda8f98f15e5f8498a
russherm <russherm@iupui.edu>
2017-03-30 13:57:41 -0400

    Delete unneeded files

--- Marketplace.java
+++
@@ -1,9 +0,0 @@
-/**
- * RMI Interface for Online Marketplace
- * @author Russell Sherman
- *
- */
-public interface Marketplace extends java.rmi.Remote {
-	int get_id() throws java.rmi.RemoteException;
-
-}
