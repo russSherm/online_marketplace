9c3d2fe43b2d0968546ef6968e9c8af4a088bec3
russherm <russherm@iupui.edu>
2017-04-04 22:33:50 -0400

    Made changes for assignment 4

--- Users.java
+++ Users.java
@@ -3,7 +3,9 @@
  * @author Russell Sherman
  *
  */
+
+// Create Users class to create new users
+// Used to implement Administrators and Customers classes
 public interface Users {
 	void create();
-
 }
