7df051f49567dd68b526226f9f7aafafe31548ef
russherm <russherm@iupui.edu>
2017-04-04 22:22:05 -0400

    Updated Abstract Factory

--- AbstractFactory.java
+++ AbstractFactory.java
@@ -1,4 +1,13 @@
+/**
+ * AbstractFactory
+ * @author Russell Sherman
+ *
+ */
+
+
 // Ryan: Please include useful comments in each file.
+// FIX: Create Abstract Factory to create users
+// FIX: Do I need this? Am I using it incorrectly?
 public abstract class AbstractFactory {
 	abstract Users createUser(String username);
 }
