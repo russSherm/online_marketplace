9363a3a2310c06157dab38fe193afc6d0d50f682
russherm <russherm@iupui.edu>
2017-04-04 22:25:02 -0400

    Made changes for assignment 4

--- AuthorizationException.java
+++ AuthorizationException.java
@@ -9,6 +9,6 @@ public class AuthorizationException extends RuntimeException {
 	private static final long serialVersionUID = 5528415690278423524L;
 
 	public AuthorizationException(String methodName) {
-		super("Invalid Authorization - Access Denined to " + methodName + "() function!");
+		super("Sorry, you don't have access to the " + methodName + " function!");
 	}
 }
--- AuthorizationInvocationHandler.java
+++ AuthorizationInvocationHandler.java
@@ -2,7 +2,14 @@ import java.io.Serializable;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+/**
+ * AuthorizationInvocationHandler class
+ * @author Russell Sherman
+ *
+ */
+
 // Ryan: Please include useful comments in each file.
+// FIX: Create Authorization Invocation Handler used with Java Annotations
 public class AuthorizationInvocationHandler implements InvocationHandler, Serializable {
 	private static final long serialVersionUID = 6925780928377938176L;
 	private Object objectImpl;
@@ -10,13 +17,15 @@ public class AuthorizationInvocationHandler implements InvocationHandler, Serial
 	public AuthorizationInvocationHandler(Object impl) {
 	   this.objectImpl = impl;
 	}
-	 
+	// FIX: Invoke method to check Annotation in Requires Role class
+	// FIX: Create new Session
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		if (method.isAnnotationPresent(RequiresRole.class)) {
 			RequiresRole test = method.getAnnotation(RequiresRole.class);
 			Session session = (Session) args[0];
 			
+			// FIX: If user role type is correct, proceed. If not, throw Authorization Exception.
 			if (session.getUser().getRoleType().equals(test.value())) {
 				 return method.invoke(objectImpl, args);
             } else {
