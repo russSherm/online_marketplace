d31d704879fb89c4a5dc619f32c1289b18953703
russherm <russherm@iupui.edu>
2017-04-04 22:23:49 -0400

    Updated with comments

--- Customers.java
+++ Customers.java
@@ -1,4 +1,11 @@
+/**
+ * Customers class
+ * @author Russell Sherman
+ *
+ */
 
+// Create Customers class to add new customer users
+// Is this still needed?
 public class Customers implements Users {
 	//Create Customers
 	@Override
