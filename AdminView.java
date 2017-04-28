0b85e39793231c9dd0503bf5bb78c711d32783bb
russherm <russherm@iupui.edu>
2017-04-28 14:38:07 -0400

    Assignment 5 minor revisions

--- AdminView.java
+++ AdminView.java
@@ -15,7 +15,7 @@ public class AdminView {
 	Session session = null;
 	
 	// Create showView for administrator interaction
-	public void showView() throws InterruptedException {
+	public boolean showView() throws InterruptedException {
 		System.out.println("Welcome back, Administrator!");
 		Thread.sleep(1000);
 	
@@ -36,80 +36,85 @@ public class AdminView {
 		
 		int selectTask;
 		
-		System.out.println("What would you like to do?");
-		Thread.sleep(1000);
-		System.out.println("1.Add Item\n2.Browse Items\n3.Purchase Item\n4.Remove Item\n5.Update Item");
-		Thread.sleep(1000);
-		System.out.println("Which number would you like to do?");
-		selectTask = input.nextInt();
+		while (true){
+			System.out.println("What would you like to do?");
+			Thread.sleep(1000);
+			System.out.println("1.Add Item\n2.Browse Items\n3.Purchase Item\n4.Remove Item\n5.Update Item\n6.Quit");
+			Thread.sleep(1000);
+			System.out.println("Which number would you like to do?");
+			selectTask = input.nextInt();
 		
-		// Create new Items instance
-		Items itemArray = new Items();
+			// Create new Items instance
+			Items itemArray = new Items();
 		
-		// Concrete Commands
-		AddItem addItem = new AddItem(itemArray);
-		RemoveItem removeItem = new RemoveItem(itemArray);
-		UpdateItem updateItem = new UpdateItem(itemArray);
+			// Concrete Commands
+			AddItem addItem = new AddItem(itemArray);
+			RemoveItem removeItem = new RemoveItem(itemArray);
+			UpdateItem updateItem = new UpdateItem(itemArray);
 		
-		// Invoker Object...
-	    Clerk clerk = new Clerk();
+			// Invoker Object...
+			Clerk clerk = new Clerk();
 		
-		// Throw exceptions for Purchase and Browse
-		if (selectTask==5){
-			try{
-				System.out.println(server.update(session));	
-			} catch(Exception e){
-				System.out.println("Client Exception: " + e.getMessage());
+			// Throw exceptions for Purchase and Browse
+			if (selectTask==5){
+				try{
+					System.out.println(server.update(session));	
+				} catch(Exception e){
+					System.out.println("Client Exception: " + e.getMessage());
+				}
+				@SuppressWarnings("resource")
+				Scanner scan5 = new Scanner(System.in);
+				String selectItem = scan5.nextLine();
+				itemArray.update(selectItem);
+				clerk.takeInitiate(updateItem);
 			}
-			@SuppressWarnings("resource")
-			Scanner scan5 = new Scanner(System.in);
-			String selectItem = scan5.nextLine();
-			itemArray.update(selectItem);
-			clerk.takeInitiate(updateItem);
-		}
 		
-		else if (selectTask==1){
-			try{
-				System.out.println(server.add(session));
-			} catch(Exception e){
-				System.out.println("Client Exception: " + e.getMessage());
+			else if (selectTask==1){
+				try{
+					System.out.println(server.add(session));
+				} catch(Exception e){
+					System.out.println("Client Exception: " + e.getMessage());
+				}
+				clerk.takeInitiate(addItem);
+				@SuppressWarnings("resource")
+				Scanner scan1 = new Scanner(System.in);
+				String selectItem = scan1.nextLine();
+				itemArray.add(selectItem);
 			}
-			clerk.takeInitiate(addItem);
-			@SuppressWarnings("resource")
-			Scanner scan1 = new Scanner(System.in);
-			String selectItem = scan1.nextLine();
-			itemArray.add(selectItem);
-		}
 		
-		else if (selectTask==3){
-			try{
-				System.out.println(server.purchase(session));
-			} catch(Exception e){
-				System.out.println("Client Exception: " + e.getMessage());
+			else if (selectTask==3){
+				try{
+					System.out.println(server.purchase(session));
+				} catch(Exception e){
+					System.out.println("Client Exception: " + e.getMessage());
+				}
 			}
-		}
 	
-		else if (selectTask==2){
-			try{
-				System.out.println(server.browse(session));
-			} catch(Exception e){
-				System.out.println("Client Exception: " + e.getMessage());
+			else if (selectTask==2){
+				try{
+					System.out.println(server.browse(session));
+				} catch(Exception e){
+					System.out.println("Client Exception: " + e.getMessage());
+				}
 			}
-		}
 		
-		else if (selectTask==4){
-			try{
-				System.out.println(server.remove(session));
-			} catch(Exception e){
-				System.out.println("Client Exception: " + e.getMessage());
+			else if (selectTask==4){
+				try{
+					System.out.println(server.remove(session));
+				} catch(Exception e){
+					System.out.println("Client Exception: " + e.getMessage());
+				}
+				@SuppressWarnings("resource")
+				Scanner scan4 = new Scanner(System.in);
+				String selectItem = scan4.nextLine();
+				itemArray.remove(selectItem);
+				clerk.takeInitiate(removeItem);
 			}
-			@SuppressWarnings("resource")
-			Scanner scan4 = new Scanner(System.in);
-			String selectItem = scan4.nextLine();
-			itemArray.remove(selectItem);
-			clerk.takeInitiate(removeItem);
+		
+			else if (selectTask==6){
+				return false;
+			}
 		}
-	
 	}
 	
 }
--- CustomerView.java
+++ CustomerView.java
@@ -16,7 +16,7 @@ public class CustomerView {
 	//private static String[] abcItem  = {"Socks", "Shoes", "Shirt", "Pants", "Toothbrush", "Chair", "Laptop", "Shampoo", "Soap"};
 	
 	// Create showView for customer interaction
-	public void showView() throws InterruptedException {
+	public boolean showView() throws InterruptedException {
 		System.out.println("Welcome to the Marketplace, Customer!");
 		Thread.sleep(1000);
 		
@@ -37,70 +37,76 @@ public class CustomerView {
 		
 		int selectTask;
 		
-		// Ask customer what action they would like to take
-		System.out.println("How can we help you?");
-		Thread.sleep(1000);
-		System.out.println("1.Add Item\n2.Browse Items\n3.Purchase Item\n4.Remove Item\n5.Update Item");
-		Thread.sleep(1000);
-		System.out.println("Which number would you like to do?");
-		selectTask = input.nextInt();
+		while (true){
+			// Ask customer what action they would like to take
+			System.out.println("How can we help you?");
+			Thread.sleep(1000);
+			System.out.println("1.Add Item\n2.Browse Items\n3.Purchase Item\n4.Remove Item\n5.Update Item\n6.Quit");
+			Thread.sleep(1000);
+			System.out.println("Which number would you like to do?");
+			selectTask = input.nextInt();
 		
-		// Create new Items instance
-		Items abcItem = new Items();
+			// Create new Items instance
+			Items abcItem = new Items();
 				
-		// Concrete Commands
-		BrowseItem browseItem = new BrowseItem(abcItem);
-		PurchaseItem purchaseItem = new PurchaseItem(abcItem);
+			// Concrete Commands
+			BrowseItem browseItem = new BrowseItem(abcItem);
+			PurchaseItem purchaseItem = new PurchaseItem(abcItem);
 				
-		// Invoker Object...
-		Clerk clerk = new Clerk();
+			// Invoker Object...
+			Clerk clerk = new Clerk();
 		
-		// Throw exceptions for Update, Add, Remove
-		if (selectTask==5){
-			try{
-				System.out.println(server.update(session));	
-			} catch(Exception e){
-				System.out.println("Client Exception: " + e.getMessage());
+			// Throw exceptions for Update, Add, Remove
+			if (selectTask==5){
+				try{
+					System.out.println(server.update(session));	
+				} catch(Exception e){
+					System.out.println("Client Exception: " + e.getMessage());
+				}
 			}
-		}
 		
-		else if (selectTask==1){
-			try{
-				System.out.println(server.add(session));
-			} catch(Exception e){
-				System.out.println("Client Exception: " + e.getMessage());
+			else if (selectTask==1){
+				try{
+					System.out.println(server.add(session));
+				} catch(Exception e){
+					System.out.println("Client Exception: " + e.getMessage());
+				}
 			}
-		}
 		
-		else if (selectTask==3){
-			try{
-				System.out.println(server.purchase(session));
-			} catch(Exception e){
-				System.out.println("Client Exception: " + e.getMessage());
+			else if (selectTask==3){
+				try{
+					System.out.println(server.purchase(session));
+				} catch(Exception e){
+					System.out.println("Client Exception: " + e.getMessage());
+				}
+				@SuppressWarnings("resource")
+				Scanner scan3 = new Scanner(System.in);
+				String selectItem = scan3.nextLine();
+				abcItem.purchase(selectItem);
+				clerk.takeInitiate(purchaseItem);
 			}
-			@SuppressWarnings("resource")
-			Scanner scan3 = new Scanner(System.in);
-			String selectItem = scan3.nextLine();
-			abcItem.purchase(selectItem);
-			clerk.takeInitiate(purchaseItem);
-		}
 	
-		else if (selectTask==2){
-			try{
-				System.out.println(server.browse(session));
-			} catch(Exception e){
-				System.out.println("Client Exception: " + e.getMessage());
-			}
-			abcItem.browse();
-			clerk.takeInitiate(browseItem);
+			else if (selectTask==2){
+				try{
+					System.out.println(server.browse(session));
+				} catch(Exception e){
+					System.out.println("Client Exception: " + e.getMessage());
+				}
+				abcItem.browse();
+				clerk.takeInitiate(browseItem);
 			
-		}
+			}
 		
-		else if (selectTask==4){
-			try{
-				System.out.println(server.remove(session));
-			} catch(Exception e){
-				System.out.println("Client Exception: " + e.getMessage());
+			else if (selectTask==4){
+				try{
+					System.out.println(server.remove(session));
+				} catch(Exception e){
+					System.out.println("Client Exception: " + e.getMessage());
+				}
+			}
+			
+			else if (selectTask==6){
+				return false;
 			}
 		}
 	}
